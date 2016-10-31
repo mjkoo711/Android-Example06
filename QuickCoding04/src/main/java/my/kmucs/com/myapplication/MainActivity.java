6227b7e1f598519ef22130e3b5b8db7aabb2b4e3
Koo Min Jun <jooon0711@kookmin.ac.kr>
2016-11-01 00:59:39 +0900

    내 위치 찍기 새로만듬, 두잇 교재참고, 1차 완료

--- QuickCoding04/src/main/java/my/kmucs/com/myapplication/MainActivity.java
+++ QuickCoding04/src/main/java/my/kmucs/com/myapplication/MainActivity.java
@@ -1,51 +1,141 @@
 package my.kmucs.com.myapplication;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+/*
+* 위치 관리자를 이용해 내 위치를 확인하는 방법
+ */
+
 public class MainActivity extends Activity {
-
-    private Button btnShowLocation;
-    private TextView txtLat;
-    private TextView txtLon;
-
-    //GpsTracker class
-    private GpsInfo gps;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnShowLocation = (Button)findViewById(R.id.btn_start);
-        txtLat = (TextView)findViewById(R.id.Latitude);
-        txtLon = (TextView)findViewById(R.id.Longitude);
+        //버튼 이벤트 처리
 
-        //Gps 정보를 보여주기 위한 이벤트 클래스 등록
-        btnShowLocation.setOnClickListener(new View.OnClickListener(){
+        Button btn01 = (Button)findViewById(R.id.btn01);
+        btn01.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                gps = new GpsInfo(MainActivity.this);
-                //gps 사용유무 가져오기
-                if(gps.isGetLocation()){
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    txtLat.setText(String.valueOf(latitude));
-                    txtLon.setText(String.valueOf(longitude));
-
-                    Toast.makeText(getApplicationContext(),"당신의 위치 - \n위도:" + latitude + "\n경도: " + longitude, Toast.LENGTH_LONG).show();
-                }
-                else{
-                    //gps를 사용할 수 없으므로
-                    gps.showSettingsAlert();
-                }
+                //위치 정보 확인을 위해 정의한 메소드 호출
+                startLocationService();
             }
         });
+
+        checkDangerousPermissions();
+
     }
+    private void checkDangerousPermissions(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for(int i=0 ; i< permissions.length; i++){
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if(permissionCheck == PackageManager.PERMISSION_DENIED){
+                break;
+            }
+        }
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,permissions[0])){
+                Toast.makeText(this,"권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            }else{
+                ActivityCompat.requestPermissions(this,permissions,1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == 1){
+            for(int i=0 ; i<permissions.length ; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, permissions[i] + "권한이 승인됨.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(this,permissions[i] + "권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    //위치 정보 확인을 위해 정의한 메소드
+    private void startLocationService(){
+        //위치 관리자 객체 참조
+        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        //위치 정보를 받을 리스너 생성
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+        try{
+            //gps를 이용한 위치 요청
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance,gpsListener);
+            //네트워크를 이용한 위치 요청
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, gpsListener);
+            //위치 확인이 안되는 경우에도 최근에 확인된 위치 정보 먼저 확인
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastLocation != null){
+                Double latitude = lastLocation.getLatitude();
+                Double longitude = lastLocation.getLongitude();
+
+                Toast.makeText(getApplicationContext(), "Last Known Location : " + "Latitude : " + latitude + "\nLongitude:" + longitude, Toast.LENGTH_LONG).show();
+            }
+        }catch (SecurityException ex){
+            ex.printStackTrace();
+        }
+
+        Toast.makeText(getApplicationContext(), "위치 확인이 시작되었습니다. 로그를 확인하세요.", Toast.LENGTH_SHORT).show();
+    }
+    private class GPSListener implements LocationListener {
+        //위치 정보가 확인될때 자동 호출되는 메소드
+        @Override
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String msg = "Latitude : " + latitude + "\nLongitude : " + longitude;
+            Log.i("GPSListner", msg);
+
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
 }
